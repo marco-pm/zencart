9cf89b834b4df31647ca4a2f3b3e8ca6c0141201
Marco Ponchia <marco.ponchia@marcopm.net>
2023-01-03 10:00:19 +0100

    Instant Search changes

---
+++ includes/templates/bootstrap/jscript/jscript_instant_search.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package  Instant Search Plugin for Zen Cart
+ * @author   marco-pm
+ * @version  3.0.0
+ * @see      https://github.com/marco-pm/zencart_instantsearch
+ * @license  GNU Public License V2.0
+ */
+
+if (defined('INSTANT_SEARCH_DROPDOWN_ENABLED') && defined('INSTANT_SEARCH_PAGE_ENABLED')) { ?>
+    <script>
+        const instantSearchDropdownEnabled        = <?php echo (bool)INSTANT_SEARCH_DROPDOWN_ENABLED; ?>;
+        const instantSearchPageEnabled            = <?php echo (bool)INSTANT_SEARCH_PAGE_ENABLED; ?>;
+        const instantSearchDropdownInputWaitTime  = parseInt(<?php echo INSTANT_SEARCH_DROPDOWN_INPUT_WAIT_TIME; ?>);
+        const instantSearchDropdownInputMinLength = parseInt(<?php echo INSTANT_SEARCH_DROPDOWN_MIN_WORDSEARCH_LENGTH; ?>);
+        const instantSearchDropdownInputSelector  = '<?php echo str_replace("'", "\'", INSTANT_SEARCH_DROPDOWN_INPUT_BOX_SELECTOR); ?>';
+    </script>
+    <script src="<?php echo $template->get_template_dir('instant_search.js', DIR_WS_TEMPLATE, $current_page_base, 'jscript') . '/' . 'instant_search.js'; ?>" defer></script>
+<?php }
